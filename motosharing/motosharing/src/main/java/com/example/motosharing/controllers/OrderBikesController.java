90b93f43d73e16074f58a7fef0e22267b08ba612
Emilly Pavlovici <emily.pavlovici@student.upt.ro>
2022-05-25 22:08:29 +0300

    Bike reservation progress
    git commit -m "ST-10 <Customers can reserve up to 10 bikes for maximum 12 hours. If quantity of bikes field or time field is empty, error message is displayed. git commit -m >"

--- motosharing/motosharing/src/main/java/com/example/motosharing/controllers/OrderBikesController.java
+++ motosharing/motosharing/src/main/java/com/example/motosharing/controllers/OrderBikesController.java
@@ -1,24 +1,85 @@
 package com.example.motosharing.controllers;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-//import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 
 public class OrderBikesController extends MainAppController
 {
 
-   /* @FXML
-    private ComboBox<String> bikeType;
+    @FXML
+    private ComboBox<String> type;
 
     @FXML
     private ComboBox<String> quantity;
 
     @FXML
-    private ComboBox<String> time;*/
+    private ComboBox<String> time;
+
+    private String[] bikeTypes= new String[10];
+    private String[] nrOfBikes= new String[10];
+    private String[] hours = new String[12];
+
+    private void setValues()
+    {
+        for(int i=0; i<12; i++)
+        {
+            /*if(i<10)
+            {
+                bikeTypes[i] = "";
+                bikeTypes[i] += (i+1);
+            }*/
+            if(i<10)
+            {
+                nrOfBikes[i] = "";
+                nrOfBikes[i] += (i+1);
+            }
+            if(i<12)
+            {
+                hours[i] = "";
+                hours[i] += (i+1);
+            }
+        }
+    }
+    @FXML
+    private void initialize()
+    {
+        setValues();
+        type.setItems(FXCollections.observableArrayList(bikeTypes));
+        type.setValue("Type");
+
+        quantity.setItems(FXCollections.observableArrayList(nrOfBikes));
+        quantity.setValue("Quantity");
+
+        time.setItems(FXCollections.observableArrayList(hours));
+        time.setValue("Time");
+    }
+
 
     @FXML
     private void handleConfirm()
     {
-        mainApp.showRateExperienceScreen();
+        String errorMessage="";
+
+        if (quantity.getValue().equals("Quantity")) {
+            errorMessage += "Quantity is empty.\n";
+        }
+
+        if (time.getValue().equals("Time")) {
+            errorMessage += "Time is empty.\n";
+        }
+
+        if (errorMessage != "") {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.initOwner(mainApp.getPrimaryStage());
+            alert.setTitle("Invalid Fields");
+            alert.setHeaderText("Please correct invalid fields");
+            alert.setContentText(errorMessage);
+
+            alert.showAndWait();
+        }
+        else
+            mainApp.showRateExperienceScreen();
     }
 
     @FXML
