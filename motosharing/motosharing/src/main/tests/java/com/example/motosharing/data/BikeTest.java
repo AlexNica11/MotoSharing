d68e9c16d763dd3d92e3665ea30a5a4649e0b571
AlexNica <alexandru.nica2@student.upt.ro>
2022-05-26 03:06:20 +0300

    Bike test almost implemented

--- motosharing/motosharing/src/main/tests/java/com/example/motosharing/data/BikeTest.java
+++ motosharing/motosharing/src/main/tests/java/com/example/motosharing/data/BikeTest.java
@@ -12,7 +12,8 @@ class BikeTest {
 
     @BeforeEach
     void setUp(){
-        bike= new Bike();
+        bike= new Bike("man11", "model", "color", "engineSize");
+        bike.setLocation(new Locations("city", "stret", "streetNumber"));
     }
 
     @Test
@@ -25,7 +26,6 @@ class BikeTest {
     @Test
     @DisplayName("Manufacturer has to be set")
     void setManufacturer() {
-        bike= new Bike("man11", "model", "color", "engineSize");
         bike.setManufacturer("manufacturer");
         assertEquals("manufacturer", bike.getManufacturer(), "The manufacturer text has to be shown");
     }
@@ -33,47 +33,70 @@ class BikeTest {
     @Test
     @DisplayName("Check if manufacturer property is got")
     void getManufacturerProperty() {
-
+        assertEquals("manufacturer", bike.getManufacturerProperty().get(), "The manufacturer text has to be shown");
     }
 
     @Test
+    @DisplayName("Check if model is got")
     void getModel() {
+        assertEquals("model", bike.getModel(), "The model text has to be shown");
     }
 
     @Test
+    @DisplayName("Check if model has been set")
     void setModel() {
+        bike.setModel("fiesta");
+        assertEquals("fiesta", bike.getModel(), "The fiesta text has to be shown");
     }
 
     @Test
+    @DisplayName("Check if model property has been got")
     void getModelProperty() {
+        assertEquals("model", bike.getModelProperty().get(), "The model text has to be shown");
     }
 
     @Test
+    @DisplayName("Check if color is got")
     void getColor() {
+        assertEquals("color", bike.getColor(), "The color text has to be shown");
     }
 
     @Test
+    @DisplayName("Check if color has been set")
     void setColor() {
+        bike.setColor("blue");
+        assertEquals("blue", bike.getColor(), "The blue text has to be shown");
     }
 
     @Test
+    @DisplayName("Check if color property has been got")
     void getColorProperty() {
+        assertEquals("color", bike.getColorProperty().get(), "The color text has to be shown");
     }
 
     @Test
+    @DisplayName("Check if engineSize is got")
     void getEngineSize() {
+        assertEquals("engineSize", bike.getEngineSize(), "The engineSize text has to be shown");
     }
 
     @Test
+    @DisplayName("Check if engineSize has been set")
     void setEngineSize() {
+        bike.setEngineSize("1200");
+        assertEquals("1200", bike.getEngineSize(), "The 1200 text has to be shown");
     }
 
     @Test
+    @DisplayName("Check if engineSize property has been got")
     void getEngineSizeProperty() {
+        assertEquals("engineSize", bike.getEngineSizeProperty().get(), "The engineSize text has to be shown");
     }
 
     @Test
+    @DisplayName("Check if location is got")
     void getLocation() {
+        assertEquals("city", bike.getLocation(), "The engineSize text has to be shown");
     }
 
     @Test
